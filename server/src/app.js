761bacf815c7b3086bd7f8ef6dce977f1a3d28bf
Alhuar <alfonsohuescar@gmail.com>
2018-12-29 20:09:28 +0100

    dev

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,12 @@
 const fastify = require('fastify')({logger: true})
-const httpStatus = require('http-status')
-
-
+require('dotenv').config()
 fastify.register(require('./routes/status'))
 
-fastify.listen(3000, (err, address) => {
+fastify.listen(process.env.BACKEND_SERVER_PORT, (err, address) => {
+  console.log('ADRESSS', address)
   if (err) {
     fastify.log.error(err)
-    process.exit(1)
+    throw err
   }
   fastify.log.info(`server listening on ${address}`)
 })
